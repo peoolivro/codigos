68f1d7ce5b9e816b930f26a570523996df596121
fabioprocopio <fabio.procopio@ifrn.edu.br>
2021-09-11 17:31:58 -0300

    Renomeação de arquivos

---
+++ Cap2_Exercicios/PEOO_Cap2_ExercicioProposto01.py
@@ -0,0 +1,10 @@
+# Livro...: Introdução a Python com Aplicações de Sistemas Operacionais
+# Capítulo: 02
+# Questão.: Exercício Proposto 1
+# Autor...: Fábio Procópio
+# Data....: 18/02/2019
+
+base = float(input("Base do triângulo (cm): "))
+alt  = float(input("Altura do triângulo (cm): "))
+area = (base * alt) / 2
+print(f"Área = {area:.2f} cm²")
